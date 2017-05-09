aa9de7b71f4fb0db2783e9c79f69bd19ccf48b83
ronkeizer <ronkeizer@gmail.com>
2017-05-09 12:10:13 -0700

    small fix

--- R/read_saved_results.R
+++ R/read_saved_results.R
@@ -1,5 +1,5 @@
 #' Read info from folder with saved runs
-#' 
+#'
 #' @param folder main folder where data is saved (default `/data`)
 #' @param user username
 #' @param read_all read all data (`TRUE``) or only header information (`FALSE`, default) from output data
@@ -16,18 +16,21 @@ read_saved_results <- function(
     all <- dir(full_folder)
     if(length(all) > 0) {
       for(i in seq(all)) {
-        sub_folder <- paste0(full_folder, '/', all[i])
-        tmp <- dir(sub_folder)
-        if("header.txt" %in% tmp) {
-          if(read_all) {
-            obj <- tb_read_all_output()
-          } else {
-            obj <- tb_read_output(sub_folder, "header", output_folder = FALSE)
+        if(!(all[i] %in% c("drugs", "settings", "therapy"))) {
+          print(all[i])
+          sub_folder <- paste0(full_folder, '/', all[i])
+          tmp <- dir(sub_folder)
+          if("header.txt" %in% tmp) {
+            if(read_all) {
+              obj <- tb_read_all_output()
+            } else {
+              obj <- tb_read_output(sub_folder, "header", output_folder = FALSE)
+            }
+            runs[[all[i]]] <- obj
           }
-          runs[[all[i]]] <- obj
         }
       }
-    } 
+    }
     if(length(runs) == 0) {
       message(paste0("No results for user ", user, " were found"))
     }
