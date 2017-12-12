8fff2de7c8defcce6aeb5d61c2fcfe5e89d086aa
ronkeizer <ronkeizer@gmail.com>
2017-12-12 09:47:20 -0800

    add mems R function

---
+++ R/create_mems_data.R
@@ -0,0 +1,54 @@
+#' Create a matrix of MEMS data for use in TBsim from observed MEMS data
+#' The function basically fills in any missing days when, and can
+#' randomly draw more patients from the dataset.
+#'
+#' @param data Observed MEMS data (as list). See example.
+#' @param n_patients number of patients to output
+#' @param n_events number of days/dosing events per patient
+#' @param random randomize the draw of patients?
+#' @param seed randomization seed
+#' @param file if not `NULL`, will write to specified CSV file
+#'
+#' @example
+#' mems_data <- list(
+#'   "a" = c(1,1,1,1,1,0,1,0,0,1,1,1,1,0,1),
+#'   "b" = c(,1,1,0,1,0,1,1,1,1,1,0,1,1,1,0,1,1,0))
+#' )
+#' create_mems_data(mems_data, n_patients = 100, random = TRUE)
+create_mems_data <- function(
+  data = NULL,
+  n_patients = NULL,
+  n_events = 100,
+  random = TRUE,
+  seed = NULL,
+  file = NULL,
+  filetype = "tbsim"
+) {
+  if(is.null(data) || !("list" %in% class(mems_data))) stop("MEMS data needed, specified as list object.")
+  if(is.null(n_patients)) n_patients <- length(data)
+  fill <- function(vec, n) {
+    rep(vec, ceiling(n/length(vec)))[1:n]
+  }
+  tmp <- matrix(unlist(lapply(data, "fill", n_events)), ncol = n_events, byrow=TRUE)
+  id <- 1:n_patients
+  if(n_patients > length(data)) {
+    tmp <- tmp[rep(1:nrow(tmp), ceiling(n_patients/nrow(tmp)))[1:n_patients],]
+  } else {
+    tmp <- tmp[1:n_patients,]
+  }
+  if(random) {
+    if(!is.null(seed)) {
+      set.seed(seed)
+    }
+    tmp <- tmp[floor(runif(n_patients, min = 1, max = n_patients+1)),]
+  }
+  if(!is.null(file)) {
+    if(tolower(filetype) == "tbsim") {
+      write.table(tmp, file = file, quote = F, row.names = F)
+    } else {
+      write.csv(tmp, file = file, quote = F, row.names = F)
+    }
+  } else {
+    return(tmp)
+  }
+}
